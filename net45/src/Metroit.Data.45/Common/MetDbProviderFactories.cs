fcc3a9498cca186360b286ff5104ed420d03a7f7
takiru <cherrysunburst@gmail.com>
2018-11-02 17:36:19 +0900

    using整理

--- net45/src/Metroit.Data.45/Common/MetDbProviderFactories.cs
+++ net45/src/Metroit.Data.45/Common/MetDbProviderFactories.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Metroit.Data.Common
 {
--- net45/src/Metroit.Data.45/Extensions/DbCommandExtensions.cs
+++ net45/src/Metroit.Data.45/Extensions/DbCommandExtensions.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Metroit.Data.Extensions
 {
