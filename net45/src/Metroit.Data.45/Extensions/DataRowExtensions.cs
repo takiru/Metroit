8a25e96d413e16d35babb36dce49e3934e8f564a
takiru <cherrysunburst@gmail.com>
2021-12-10 16:03:33 +0900

    Metroit.Data.45 1.2.4 Released.

--- net45/src/Metroit.Data.45/Extensions/DataRowExtensions.cs
+++ net45/src/Metroit.Data.45/Extensions/DataRowExtensions.cs
@@ -15,8 +15,9 @@ namespace Metroit.Data.Extensions
         /// </summary>
         /// <typeparam name="T">エンティティクラス。</typeparam>
         /// <param name="dataRow">DataRow オブジェクト。</param>
+        /// <param name="shouldImport">値を設定すべきかどうかを判断する式。</param>
         /// <returns>エンティティクラスオブジェクト。</returns>
-        public static T ToEntity<T>(this DataRow dataRow) where T : new()
+        public static T ToEntity<T>(this DataRow dataRow, Func<ShouldDataRowImportArgs, bool> shouldImport = null) where T : new()
         {
             var resultData = new T();
             var t = resultData.GetType();
@@ -26,7 +27,7 @@ namespace Metroit.Data.Extensions
             {
                 var columnName = pi.Name;
 
-                // PhysicalNameAttribute が設定されている場合、そのカラム名を使用する
+                // ColumnAttribute が設定されている場合、そのカラム名を使用する
                 if (Attribute.GetCustomAttribute(pi, typeof(ColumnAttribute)) is ColumnAttribute attr)
                 {
                     columnName = attr.Name;
@@ -34,6 +35,12 @@ namespace Metroit.Data.Extensions
 
                 // 列名が存在しない場合は値の設定を行わない
                 if (!dataRow.Table.Columns.Contains(columnName))
+                {
+                    continue;
+                }
+
+                // 設定すべきでない列はスキップする
+                if (!(shouldImport?.Invoke(new ShouldDataRowImportArgs(columnName, dataRow[columnName])) ?? true))
                 {
                     continue;
                 }
@@ -67,7 +74,8 @@ namespace Metroit.Data.Extensions
         /// </summary>
         /// <param name="dataRow">DataRow オブジェクト。</param>
         /// <param name="entity">エンティティクラスオブジェクト。</param>
-        public static void FromEntity(this DataRow dataRow, object entity)
+        /// <param name="shouldImport">値を設定すべきかどうかを判断する式。</param>
+        public static void FromEntity(this DataRow dataRow, object entity, Func<ShouldDataRowImportArgs, bool> shouldImport = null)
         {
             var columns = dataRow.Table.Columns;
             var t = entity.GetType();
@@ -90,6 +98,13 @@ namespace Metroit.Data.Extensions
                 }
 
                 var value = pi.GetValue(entity, null);
+
+                // 設定すべきでない列はスキップする
+                if (!(shouldImport?.Invoke(new ShouldDataRowImportArgs(columnName, value)) ?? true))
+                {
+                    continue;
+                }
+
                 if (value == null)
                 {
                     dataRow[columnName] = DBNull.Value;
---
+++ net45/src/Metroit.Data.45/Extensions/ShouldDataRowImportArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metroit.Data.Extensions
+{
+    /// <summary>
+    /// DataRow へのデータ設定判断用の情報を提供します。
+    /// </summary>
+    public class ShouldDataRowImportArgs
+    {
+        /// <summary>
+        /// 項目名を取得します。
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// 項目値を取得します。
+        /// </summary>
+        public object Value { get; }
+
+        /// <summary>
+        /// 新しい ShouldDataRowImportArgs インスタンスを生成します。
+        /// </summary>
+        /// <param name="name">項目名。</param>
+        /// <param name="value">項目値。</param>
+        public ShouldDataRowImportArgs(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
