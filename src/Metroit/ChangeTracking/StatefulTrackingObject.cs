2e46d858e6c1517718b2dd35a3213358406909e6
takiru0105 <cherrysunbrst@gmail.com>
2025-07-23 15:01:53 +0900

    Abolished   - IItemRemovedKnownList   - ItemRemovedKnownList
    Reviewed
      - PropertyChangeTracker
    
    Added
      - IPropertyChangeTrackerProvider
      - ITrackingItem
      - IRemoveTrackingList
      - TrackingList
      - TrackingObject
      - StatefulTrackingObject

--- src/Metroit/ChangeTracking/StatefulTrackingObject.cs
+++ src/Metroit/ChangeTracking/StatefulTrackingObject.cs
@@ -2,7 +2,7 @@
 
 namespace Metroit.ChangeTracking
 {
-    public class TrackingObjectWithState<T> : TrackingObject<T>, IStateObject where T : class
+    public class StatefulTrackingObject<T> : TrackingObject<T>, IStateObject where T : class
     {
         private ItemState _state = ItemState.New;
 
@@ -15,7 +15,7 @@ namespace Metroit.ChangeTracking
         /// <summary>
         /// 新しいインスタンスを生成します。
         /// </summary>
-        public TrackingObjectWithState() : base() { }
+        public StatefulTrackingObject() : base() { }
 
         /// <summary>
         /// 状態を変更します。
